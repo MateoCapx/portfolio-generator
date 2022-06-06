80b6017cda455057960ddbc6eabf515fc2606852
Mateo <mateom.realtor@gmail.com>
2022-06-06 11:16:03 -0400

    updated script

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
-const fs = require('fs');// Required in order to use fs module
-const generatePage = require('Github/portfolio-generator/src/page-template.js');
+const inquirer = require('inquirer')
 
-const profileDataArgs = process.argv.slice(2);
+inquirer 
+  .prompt([
+    {
 
-console.log(profileDataArgs);
+      type: 'input',
+      name: 'name',
+      message: 'What is your team?'
 
-const [name, github] = profileDataArgs;  // Example of assignment destructuring
 
-console.log(name, github);
+    }
 
-const pageHTML = generatePage(Mateo, github);
+  ])
+.then(answers => console.log(answers));
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
 
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs'); // Required in order to use fs module
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+
+
+
+// const profileDataArgs = process.argv.slice(2); //The process.argv property returns an array containing the command-line arguments passed when the Node.js process was launched.
